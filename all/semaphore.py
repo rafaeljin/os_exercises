1ff67b27e3c8b8cb10144324f3819219debfff98
Rafael Jin <rafaeljin@users.noreply.github.com>
2016-04-28 18:03:52 +0800

    id

--- all/semaphore.py
+++ all/semaphore.py
@@ -2,46 +2,51 @@
 
 import threading  
 import time   
-   
+
+south_bridge = -1
+north_bridge = -1
+middle1 = -1
+middle2 = -1
+
 class Person(threading.Thread):  
 
-    def __init__(self,load,north,south,output):
-        threading.Thread.__init__(self)  
+    def __init__(self,idnumber,load,north,south,output):
+        threading.Thread.__init__(self)
+        self.idno = idnumber
         self.load  = load
         self.north = north
         self.south = south
         self.output = output
         
     def go_south(self):
-        load.acquire()
-        north.acquire()
+        self.load.acquire()
+        self.north.acquire()
         self.printout("过北段桥")
         self.printout("到桥中间")
-        north.release()
-        south.acquire()
+        self.north.release()
+        self.south.acquire()
         self.printout("过南段桥")
         self.printout("到达南岸")
-        south.release()
-        load.release()
+        self.south.release()
+        self.load.release()
 
     def go_north(self):
-        load.acquire()
-        south.acquire()
+        self.load.acquire()
+        self.south.acquire()
         self.printout("过南段桥")
         self.printout("到桥中间")
-        south.release()
-        
-        north.acquire()
+        self.south.release()
+        self.north.acquire()
         self.printout("过北段桥")
         self.printout("到达北岸")
-        south.release()
-        load.release()
+        self.north.release()
+        self.load.release()
     def run(self):
         self.go_north()
 
     def printout(self,s):
         output.acquire()
-        print s
+        print str(self.idno) + ":" + s
         output.release()
       
 if __name__ == "__main__":
@@ -50,6 +55,6 @@ if __name__ == "__main__":
     south = threading.Semaphore(1)
     north = threading.Semaphore(1)
     
-for i in range(0, 2):
-    my_thread = Person(load,north,south,output)
+for i in range(0, 10):
+    my_thread = Person(i,load,north,south,output)
     my_thread.start()
